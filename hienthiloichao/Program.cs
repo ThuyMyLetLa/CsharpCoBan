527b0125e2362b000255ba247711e66d4ffa2edc
ThuyMyLetLa <dinhcongdai0310@gmail.com>
2025-11-04 23:22:34 +0700

    update bai tap
    bai tap hien thi loi chao

---
+++ hienthiloichao/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ban ten la gi?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Xin chao, {name}!");
+    }
+}
