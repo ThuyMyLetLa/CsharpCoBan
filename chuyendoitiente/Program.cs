574e0693308b4c2f91a99af5e459df460129df95
ThuyMyLetLa <dinhcongdai0310@gmail.com>
2025-11-04 23:34:56 +0700

    update bai tap
    bai tap chuyen doi tien te

---
+++ chuyendoitiente/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int usd = 23000000;
+        Console.WriteLine("Nhap so usd can doi sang vnd: ");
+        int vnd = Convert.ToInt32(Console.ReadLine());
+        int tienvnd = vnd * usd;
+        Console.WriteLine($"so tien chuyen doi la: {tienvnd} VND");
+    }
+}
